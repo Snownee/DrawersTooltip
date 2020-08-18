cf9a51df5f1dd0a51411f674e5ba04979421ab47
Snownee <1850986885@qq.com>
2020-08-18 13:03:56 +0800

    port: 1.16.2

--- src/main/java/snownee/drawer/ModTags.java
+++ src/main/java/snownee/drawer/ModTags.java
@@ -3,8 +3,9 @@ package snownee.drawer;
 import net.minecraft.item.Item;
 import net.minecraft.tags.ITag.INamedTag;
 import net.minecraft.tags.ItemTags;
+import net.minecraft.util.ResourceLocation;
 
 public final class ModTags {
-    public static final INamedTag<Item> DRAWERS = ItemTags.makeWrapperTag("storagedrawers:drawers");
-    public static final INamedTag<Item> COMPACTING_DRAWERS = ItemTags.makeWrapperTag("drawerstooltip:compacting_drawers");
+    public static final INamedTag<Item> DRAWERS = ItemTags.createOptional(new ResourceLocation("storagedrawers:drawers"));
+    public static final INamedTag<Item> COMPACTING_DRAWERS = ItemTags.createOptional(new ResourceLocation("drawerstooltip:compacting_drawers"));
 }
